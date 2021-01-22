5c08e19381816eabfd02884f74f3695efd2f94a7
Kirill Andreev <kreizi2398@mail.ru>
2021-01-22 23:37:45 +0300

    Select

--- JS/script.js
+++ JS/script.js
@@ -267,16 +267,53 @@ window.addEventListener("DOMContentLoaded", () => {
     //Select
 
     const wrappers = document.querySelectorAll(".select__wrapper");
-
-    wrappers.forEach(wrapper => {
-        wrapper.addEventListener("click", () => {
+    const arrowElem = "<div class='select__arrow'>Arrow</div>";
+    wrappers.forEach((wrapper, i) => {
+        wrapper.addEventListener("click", (e) => {
             if (wrapper.classList.contains("active")) {
                 wrapper.classList.remove("active");
+                if (e.target.classList.contains("select__item")){
+                    wrapper.querySelector(".select__title").innerHTML = 
+                        e.target.innerText + arrowElem;
+
+                    switch (i) {
+                        case 0:
+                            showSelectedRowsByMap();
+                            break;
+                    
+                        case 1:
+                            showSelectedRowsByRes();
+                            break;
+                        default:
+                            showSelectedRowsByKda()
+                            break;
+                    }
+                }
             } else {
                 wrapper.classList.add("active");
             }
         });
     });
+
+    // Reset
+
+    const reset = document.querySelector(".select__reset");
+    const selectTitles = document.querySelectorAll(".select__title");
+    reset.addEventListener("click", () => {
+        const namesOfTitle = ["Map", "Result", "K/A/D"];
+        const rows = document.querySelectorAll(".stats__row");
+
+        selectTitles.forEach((title, i) => {
+            title.innerHTML = namesOfTitle[i] + arrowElem;
+        });
+        wrappers.forEach(wrapper => {
+            wrapper.classList.remove("active");
+        });
+
+        rows.forEach(row => {
+            row.style.display = "block";
+        });
+    })
 
 
     // Row factory
@@ -291,7 +328,6 @@ window.addEventListener("DOMContentLoaded", () => {
             this.checkRes = result === "Won"? "won__span" : "lost__span";
         }
 
-        // TODO:here
         render() {
             const row = document.createElement("div");
             row.classList.add("stats__row")
@@ -329,4 +365,47 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
     renderAllRows();
+    
+
+
+    function showSelectedRowsByMap() {
+        const mapsOfRow = document.querySelectorAll(".stats__map");
+        const mapTitle = document.querySelectorAll(".select__title")[0].innerText;
+        mapsOfRow.forEach(map => {
+            const row = map.parentElement.parentElement;
+            if (!mapTitle.includes(map.innerText) && !mapTitle.includes("All")) {
+                row.style.display = "none";
+            } else {
+                row.style.display = "block";
+            }
+        });
+    }
+
+    function showSelectedRowsByRes() {
+        const resultsOfRow = document.querySelectorAll(".stats__res");
+        const resultTitle = document.querySelectorAll(".select__title")[1].innerText;
+        resultsOfRow.forEach(res => {
+            const row = res.parentElement.parentElement;
+
+            if (!resultTitle.includes(res.innerText) && !resultTitle.includes("All")) {
+                row.style.display = "none";
+            } else {
+                row.style.display = "block";
+            }
+        });
+    }
+
+    function showSelectedRowsByKda() {
+        const kdasOfRow = document.querySelectorAll(".stats__kda");
+        const kdaTitle = document.querySelectorAll(".select__title")[2].innerText;
+        kdasOfRow.forEach(kda => {
+            const row = kda.parentElement.parentElement;
+            
+            if (!kdaTitle.includes(kda.innerText) && !kdaTitle.includes("All")) {
+                row.style.display = "none";
+            } else {
+                row.style.display = "block";
+            }
+        });
+    }
 });
